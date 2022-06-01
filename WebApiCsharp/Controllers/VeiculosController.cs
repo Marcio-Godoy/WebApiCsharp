6b614b80af12748b4e17d31271fdb2abca14af33
Marcio Godoy <marciogodoybts97@gmail.com>
2022-06-01 01:22:12 -0300

    C# - Criando um fornecedor de API
    - Criando uma propriedade veiculos no controller e criando os métodos, e criando uma classe veiculos em Models além e criando suas propriedades.
    
    - Alteração no App_Start para remover o XML e somenter "ler" em JSON.

--- WebApiCsharp/Controllers/VeiculosController.cs
+++ WebApiCsharp/Controllers/VeiculosController.cs
@@ -1,24 +1,47 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApiCsharp.Models;
 
 namespace WebApiCsharp.Controllers
 {
     public class VeiculosController : ApiController
     {
-        // GET api/veiculos
-        public IEnumerable<string> Get()
+
+        public static List<Veiculos> listaVeiculos = new List<Veiculos>();
+
+        #region Métodos 
+
+        [HttpGet]
+        [Route("api/veiculos/popular")]
+        public string Popular()
         {
-            return new string[] { "value1", "value2" };
+            listaVeiculos.Add(new Veiculos(1, "FORD FIESTA", "1.0 MPI PERSNNALITÉ SEDAN 4P", 2005, 2005,
+                "Preto", 2, false, 25000, true));
+
+            listaVeiculos.Add(new Veiculos(2, "KIA CERATO", "1.6 EX3 SEDAN 16V 4P A ", 2014, 2015,
+              "Prata", 1, true, 35000, true));
+
+            listaVeiculos.Add(new Veiculos(3, "FIAT TORO", "2.0 FREEDOM 16V 4P", 2022, 2022,
+              "Vermelho", 3, true, 119590, true));
+
+            return "populado";
+        }
+
+        // GET api/veiculos
+        public string Get()
+        {
+            return JsonConvert.SerializeObject(listaVeiculos);
         }
 
         // GET api/veiculos/5
         public string Get(int id)
         {
-            return "value";
+            return JsonConvert.SerializeObject(listaVeiculos.Find(x => x.Id.Equals(id)));
         }
 
         // POST api/veiculos
@@ -31,9 +54,16 @@ namespace WebApiCsharp.Controllers
         {
         }
 
-        // DELETE api/veiculos/5
-        public void Delete(int id)
+        [HttpGet]
+        [Route("api/veiculos/excluir/{id}")]
+        public string Excluir(int id)
         {
+            var veiculo = listaVeiculos.Single(x => x.Equals(id));
+            listaVeiculos.Remove(veiculo);
+
+            return "ok";
         }
+
+        #endregion
     }
 }
