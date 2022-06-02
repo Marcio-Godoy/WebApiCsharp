57e0643edf1b04b749fb14325777cfc1d761d9be
Marcio Godoy <marciogodoybts97@gmail.com>
2022-06-01 23:31:40 -0300

    Melhoria no código e realização de testes da API utilizando Postman

--- WebApiCsharp/Controllers/VeiculosController.cs
+++ WebApiCsharp/Controllers/VeiculosController.cs
@@ -1,9 +1,7 @@
 ﻿using Newtonsoft.Json;
-using System;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using WebApiCsharp.Models;
 
@@ -18,7 +16,7 @@ namespace WebApiCsharp.Controllers
 
         [HttpGet]
         [Route("api/veiculos/popular")]
-        public string Popular()
+        public JObject Popular()
         {
             listaVeiculos.Add(new Veiculos(1, "FORD FIESTA", "1.0 MPI PERSNNALITÉ SEDAN 4P", 2005, 2005,
                 "Preto", 2, false, 25000, true));
@@ -29,19 +27,20 @@ namespace WebApiCsharp.Controllers
             listaVeiculos.Add(new Veiculos(3, "FIAT TORO", "2.0 FREEDOM 16V 4P", 2022, 2022,
               "Vermelho", 3, true, 119590, true));
 
-            return "populado";
+            var resultado = JObject.Parse("{resultado: \"populado\"}");
+            return resultado;
         }
 
         // GET api/veiculos
-        public string Get()
+        public List<Veiculos> Get()
         {
-            return JsonConvert.SerializeObject(listaVeiculos);
+            return listaVeiculos;
         }
 
         // GET api/veiculos/5
-        public string Get(int id)
+        public Veiculos Get(int id)
         {
-            return JsonConvert.SerializeObject(listaVeiculos.Find(x => x.Id.Equals(id)));
+            return listaVeiculos.Find(x => x.Id.Equals(id));
         }
 
         // POST api/veiculos
@@ -56,12 +55,13 @@ namespace WebApiCsharp.Controllers
 
         [HttpGet]
         [Route("api/veiculos/excluir/{id}")]
-        public string Excluir(int id)
+        public JObject Excluir(int id)
         {
-            var veiculo = listaVeiculos.Single(x => x.Equals(id));
+            var veiculo = listaVeiculos.Single(x => x.Id.Equals(id));
             listaVeiculos.Remove(veiculo);
 
-            return "ok";
+            var resultado = JObject.Parse("{resultado: \"ok\"}");
+            return resultado;
         }
 
         #endregion
